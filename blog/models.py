ba1a75569ccfec52544f0db89c610d2477321e63
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-25 20:40:57 -0600

    made some changes

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    #ID default auto field
+    
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    
+    publish = models.DateTimeField(default=timezone.now)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    #this is for making the django print the name of the object
+    def __str__(self):
+        return self.title
+    
