6d6cf94218d49866f9ebb1e1f106127b2fb11a56
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-28 13:39:07 -0600

    made changes to the website

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'blog'
 
-url_patterns = [
+urlpatterns = [
     path('',            views.post_list,    name='post_list'),
     path('<int:id>',    views.post_detail,  name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ def post_detail(request, id):
     
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     
-    render(
+    return render(
         request, 
         'blog/post/detail.html', 
         {'post':post}
@@ -16,7 +16,7 @@ def post_list(request):
     
     posts = Post.published.all()
     
-    render(
+    return render(
         request, 
         'blog/post/list.html', 
         {'posts':posts}
