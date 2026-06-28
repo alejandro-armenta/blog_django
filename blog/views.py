6101a77df7cb586e6b3113e1cd1115072fb85b23
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-28 17:21:25 -0600

    format code

--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 
-def post_detail(request, id):
-    
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    
-    return render(
-        request, 
-        'blog/post/detail.html', 
-        { 'post' : post }
-    )    
+
+def post_detail(request, year, month, day, post):
+
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+
+    return render(request, "blog/post/detail.html", {"post": post})
+
 
 def post_list(request):
-    
+
     posts = Post.published.all()
-    
-    return render(
-        request, 
-        'blog/post/list.html', 
-        { 'posts' : posts }
-    )
+
+    return render(request, "blog/post/list.html", {"posts": posts})
