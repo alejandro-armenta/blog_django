afc6da4dd56a69926f573bb3467b367b857153c4
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-28 21:10:43 -0600

    made form and get and post

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,30 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from .forms import EmailPostForm
 from django.views.generic import ListView
+
+
+def post_share(request, post_id):
+
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+
+    if request.method == "POST":
+        # processing email data
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            ale = form.cleaned_data
+    else:
+        # initial form
+        form = EmailPostForm()
+
+    return render(request, "blog/post/share.html", {"post": post, "form": form})
+
 
 def post_detail(request, year, month, day, post):
 
@@ -18,13 +40,14 @@ def post_detail(request, year, month, day, post):
 
     return render(request, "blog/post/detail.html", {"post": post})
 
+
 class PostListView(ListView):
-    
+
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
-    
+    template_name = "blog/post/list.html"
+
 
 def post_list(request):
 
