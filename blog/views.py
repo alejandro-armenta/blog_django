62bed9f14a72b4d43e7dbfd367ca51e7c182dc01
Alejandro <armentaayalaalejandro@gmail.com>
2026-06-29 17:35:45 -0600

    save view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
+from .models import Post
+from .forms import EmailPostForm, CommentForm
+
+
+@require_POST
+def post_comment(request, post_id):
+
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+
+        comment.post = post
+
+        comment.save()
+
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
+    )
 
 
 def post_share(request, post_id):
@@ -43,7 +69,7 @@ def post_share(request, post_id):
                 from_email=None,
                 recipient_list=[cd["to"]],
             )
-            
+
             sent = True
 
     else:
